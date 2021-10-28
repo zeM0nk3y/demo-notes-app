a3e6e51ae0e08a734becbeb9ec072ac6fad422cc
zeM0nk3y <gcoffre@zendesk.com>
2021-10-28 11:23:41 +0200

    Enabling CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -9,7 +9,19 @@ export default class StorageStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
     // Create an S3 bucket
-    this.bucket = new sst.Bucket(this, "Uploads");
+    this.bucket = new sst.Bucket(this, "Uploads", {
+      s3Bucket: {
+        // Allow client side side acces to the bucket from a different domain
+        cors: [
+          {
+            maxAge: 3000,
+            allowedOrigins: ["*"],
+            allowedHeaders: ["*"],
+            allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+          },
+        ],
+      },
+    });
     // Create the DynamoDB table
     this.table = new sst.Table(this, "Notes", {
       fields: {
