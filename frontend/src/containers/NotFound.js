86f68bc5b7b780ed21113db332ec4e03d8c676d8
zeM0nk3y <gcoffre@zendesk.com>
2021-10-29 07:38:36 +0200

    Setting up our React app

---
+++ frontend/src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound text-center">
+      <h3>Uh-Ho... you must be lost, this page does not exist!</h3>
+    </div>
+  );
+}
